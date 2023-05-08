1a4feab426a401505c808fd139f7aa0646283585
Tafadzwa Kahwai <kahwaitafadzwa@gmail.com>
2023-05-08 10:55:26 +0200

    Add project files.

---
+++ WebApplication4/Classes/LookUp.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Classes
+{
+    public class LookUp
+    {
+        //Menu objMenu;
+        protected string mMsgFlg;
+        protected Database db;
+        protected string mConnectionName;
+
+       
+
+
+
+
+        protected long mObjectUserID;
+
+        public string MsgFlg
+        {
+            get { return mMsgFlg; }
+            set { mMsgFlg = value; }
+        }
+        public Database Database
+        {
+            get { return db; }
+        }
+
+        
+
+        public string ConnectionName
+        {
+            get { return mConnectionName; }
+        }
+
+        public LookUp(string ConnectionName, long ObjectUserID)
+        {
+            mObjectUserID = ObjectUserID;
+            mConnectionName = ConnectionName;
+            db = new DatabaseProviderFactory().Create(ConnectionName);
+        }
+        public DataSet getTax(string Currency)
+        {
+            string str = "SELECT ID,CONVERT(char(10), EffectiveDate,126)EffectiveDate,BandStart,BandEnd,BandRate,Cumulative,Currency FROM TaxTables where Currency = '"+ Currency +"' and EffectiveDate=(Select Max(EffectiveDate) from TaxTables)order by EffectiveDate Desc";
+            return ReturnDs(str);
+        }
+        public DataSet TaxTables(double CumSalary, string Currency, DateTime PayDate)
+        {
+            try
+            {
+                string sql = "TaxTables_BySalary";
+                System.Data.Common.DbCommand cmd = db.GetStoredProcCommand(sql);
+
+                db.AddInParameter(cmd, "@Amount", DbType.Double, CumSalary);
+                db.AddInParameter(cmd, "@Currency", DbType.String, Currency);
+                db.AddInParameter(cmd, "@PaydateDate", DbType.Date, PayDate);
+                DataSet ds = db.ExecuteDataSet(cmd);
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    return ds;
+
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                mMsgFlg = ex.Message;
+                return null;
+            }
+        }
+
+
+        protected DataSet ReturnDs(string str)
+        {
+            try
+            {
+                DataSet ds = db.ExecuteDataSet(CommandType.Text, str);
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    return ds;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                mMsgFlg = ex.Message;
+                return null;
+            }
+        }
+    }
+}
---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication4.Classes;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.MaintainScrollPositionOnPostBack = true;
+            if (!IsPostBack)
+            {
+                
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            LookUp lp = new LookUp("con",1);
+            if (dropdownCurrency.SelectedItem.Text == "Select Currency")
+            {
+                lblError.Text = "Please select currency";
+                return;
+            }
+            lblError.Text = "";
+
+            if (txtmedicalaid.Text == "")
+            {
+                txtmedicalaid.Text = "0";
+            }
+
+            double contributionrate = 0;
+            double MedicalAid = Math.Round(double.Parse(txtmedicalaid.Text), 2);
+            double tax = 0;
+            double AidsLevy = 0;
+            double cummulativebalance = 0;
+            double GrossSalary = double.Parse(txtgrosssalary.Text);
+            double TotalTax = 0;
+            double NetSalary = 0;
+            DataSet taxtable = lp.TaxTables(GrossSalary, dropdownCurrency.SelectedItem.Text,DateTime.Now);
+            if (taxtable!=null)
+            {
+                foreach (DataRow dt in taxtable.Tables[0].Rows)
+                {
+                    cummulativebalance = Math.Round(double.Parse(dt["Cumulative"].ToString()), 2);
+                    tax = Math.Round(double.Parse(dt["BandRate"].ToString()) / 100, 2);
+                }
+            }
+            TotalTax = (GrossSalary * tax) - cummulativebalance;
+            txtTotalTax.Text = TotalTax.ToString();
+            if (CheckIsAidsLevy.Checked)
+            {
+                AidsLevy = TotalTax * .03;
+            }
+            if (CheckContributions.Checked)
+            {
+                contributionrate = .05;
+            }
+            
+            NetSalary = (GrossSalary - TotalTax) - (GrossSalary * contributionrate) - MedicalAid + AidsLevy;
+            txtnetsalary.Text = NetSalary.ToString();
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtgrosssalary.Text = "";
+            txtmedicalaid.Text = "";
+            chkIsActive.Checked = false;
+            CheckIsAidsLevy.Checked = false;
+            CheckContributions.Checked = false;
+            ma.Visible = false;
+            lblError.Text = "";
+            txtnetsalary.Text = "";
+            txtTotalTax.Text = "";
+
+        }
+
+        protected void chkIsActive_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkIsActive.Checked)
+            {
+
+                ma.Visible = true;
+            }
+            else
+            {
+                ma.Visible = false;
+                txtmedicalaid.Text = "";
+            }
+        }
+
+        protected void drpCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LookUp lp = new LookUp("con", 1);
+            DataSet tax = lp.getTax(drpCurrency.SelectedItem.Text);
+
+            if (tax!=null)
+            {
+                grdTax.DataSource = tax;
+                grdTax.DataBind();
+            }
+            else
+            {
+                grdTax.DataSource = null;
+                grdTax.DataBind();
+            }
+
+        }
+    }
+}
