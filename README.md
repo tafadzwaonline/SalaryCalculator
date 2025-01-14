0e3a4fe0f9a22f70079a53476a42ea73832bd335
Tafadzwa Kahwai <kahwaitafadzwa@gmail.com>
2025-01-14 14:02:09 +0200

    --Aids L

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SalaryCalculator
-This is a small web system where you can enter your gross salary to determine possible net salary. It was developed using ASP.Net webforms, C#, SQL and a bootstrap template. The tax tables used where taken from ZIMRA website. Please find the demo link below:
+This is a small web system where you can enter your gross salary to determine possible net salary. It was developed using .Net, C#, SQL and a bootstrap template. The tax tables used where taken from ZIMRA website. Please find the demo link below:
 
 ![Screenshot1](https://github.com/tafadzwaonline/SalaryCalculator/assets/97532736/05ce8d6f-086a-4c08-8c29-744d55cb0f1b)
 ![Screenshot 4](https://github.com/tafadzwaonline/SalaryCalculator/assets/97532736/494e9314-f709-488a-b711-6b41ff36b977)
--- WebApplication4/aids-levy.aspx.cs
+++ WebApplication4/aids-levy.aspx.cs
@@ -1,17 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication4.Classes;
 
 namespace WebApplication4
 {
     public partial class aids_levy : System.Web.UI.Page
     {
+        readonly LookUp lp = new LookUp("con", 1);
         protected void Page_Load(object sender, EventArgs e)
         {
+            MaintainScrollPositionOnPostBack = true;
+            if (!IsPostBack)
+            {
+                getRate();
+            }
+        }
 
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            double rate;
+            if (string.IsNullOrWhiteSpace(txtEffective.Text))
+            {
+                lblError.Text = "effective date is required";
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtRate.Text))
+            {
+                lblError.Text = "rate is required";
+                return;
+            }
+
+            if (!double.TryParse(txtRate.Text, out rate))
+            {
+                lblError.Text = "rate must be a valid number.";
+                return;
+            }
+
+            if (double.Parse(txtRate.Text) <= 0)
+            {
+                lblError.Text = "rate cannot be less or equal to zero";
+                return;
+            }
+
+
+
+            ///save to database
+            lp.SavePensionRate(Convert.ToDateTime(txtEffective.Text), Convert.ToDouble(txtRate.Text));
+            getRate();
+            lblSuccess.Text = "rate successfully saved";
+            Clear();
+        }
+
+
+        private void getRate()
+        {
+            DataSet rates = lp.getPensionRate();
+            if (rates != null)
+            {
+                grdRate.DataSource = rates;
+                grdRate.DataBind();
+            }
+            else
+            {
+                grdRate.DataSource = null;
+                grdRate.DataBind();
+            }
+        }
+        private void Clear()
+        {
+            txtRate.Text = string.Empty;
+            txtEffective.Text = string.Empty;
+            lblError.Text = string.Empty;
         }
     }
 }
